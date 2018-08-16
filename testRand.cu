3c749871bf837e192309992d4e72c31e9d78311f
zokaiG <futlouco7@gmail.com>
2018-08-15 23:21:11 -0300

    Saving randGen test in cuda

---
+++ testRand.cu
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <math.h>
+#include <assert.h>
+
+__global__ void randWork(unsigned int *seed, curandState_t* states)
+{
+   __shared__ int* d_rnumbs;
+   curand_init(seed[threadIdx.x],threadIdx.x,0,&states[threadIdx.x]);
+   d_rnumbs[threadIdx.x] = curand(&states[threadIdx.x])% 100;
+
+}
+
+int main(){
+
+  int nThreads = 10;
+
+  curandState_t* states;
+  unsigned int *h_seed = (unsigned int*)malloc(sizeof(unsigned int)*nThreads);
+  srand(time(NULL));
+  for(int i=0;i<nThreads;i++)
+  {
+    h_seed[i] = rand()%100000;
+  }
+
+  int *rnumbs = (int*)malloc(sizeof(int)*nThreads);
+  int *d_rnumbs = (int*)malloc(sizeof(int)*nThreads);
+  cudaMalloc((void**)&d_rnumbs, sizeof(int)*nThreads);
+
+  cudaMalloc((void**) &states, nThreads * sizeof(curandState_t));
+  unsigned int *d_seed;
+
+  cudaMalloc((void**)&d_seed, sizeof(unsigned int)*nThreads);
+  cudaMemcpy(d_seed, h_seed, sizeof(unsigned int)*nThreads,cudaMemcpyHostToDevice);
+
+  // veja somente parametros d_seed e states
+  randWork<<<1,nThreads>>>(d_seed ,states);
+
+  cudaMemcpy(d_rnumbs, rnumbs, sizeof(int)*nThreads,cudaMemcpyDeviceToHost);
+
+  cudaFree(states); cudaFree(d_seed);
+  free(h_seed);
+
+
+  printf("Random Numbers:\n");
+  for (int i = 0; i < nThreads; i++)
+    printf("%d: %d\n", i, rnumbs[i]);
+
+  return 0;
+}
